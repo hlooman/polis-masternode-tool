ae81828fcb50867a340972d54d614aab8fede557
Hans <hans@hodl.blue>
2019-10-18 13:25:29 -0700

    Connect via SSL

--- src/default_config.py
+++ src/default_config.py
@@ -4,19 +4,19 @@ polisd_default_connections = [
     {
         "use_ssh_tunnel": False,
         "host": "seattle.polis.tools",
-        "port": "8080",
+        "port": "443",
         "username": "pmtuser",
         "password": "674141414141426471647a73544a51382d344f2d394a5f4e62557a4151716548303856327a376c2d70746c796e524e54503830754d30625a54724d68524959705751555078496254475843387376306a492d534e4941364a446557456a52483743773d3d",
-        "use_ssl": False,
+        "use_ssl": True,
         "rpc_encryption_pubkey": ""
     },
     {
         "use_ssh_tunnel": False,
         "host": "frankfurt.polis.tools",
-        "port": "8080",
+        "port": "443",
         "username": "pmtuser",
         "password": "674141414141426471647a7374455242563248657177754f2d62505136494558535a37326742593534586f5838434a416f6334646d7763336f38684c525a6b35737a745751424b417757755570764d34732d74566d636e4a447579626e736c6834513d3d",
-        "use_ssl": False,
+        "use_ssl": True,
         "rpc_encryption_pubkey": ""
     }
 ]
